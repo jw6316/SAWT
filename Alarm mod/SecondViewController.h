b75892bc6ecfe926ad93332cc475ed414340ea19
Jamie Walker <Jamie@Walkers-Mac-Book-Pro.local>
2014-06-13 18:59:35 +0900

    first try XD

--- Alarm mod/SecondViewController.h
+++ Alarm mod/SecondViewController.h
@@ -1,13 +1,74 @@
 //
 //  SecondViewController.h
-//  Alarm mod
+//  alarm for golden week and etc
 //
-//  Created by Jamie Walker on 5/5/14.
+//  Created by Jamie Walker on 5/3/14.
 //  Copyright (c) 2014 Jamie Walker. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioServices.h>
+#import <AudioToolbox/AudioServices.h>
+#import <AVFoundation/AVCaptureDevice.h>
 
-@interface SecondViewController : UIViewController
+
+
+
+@interface SecondViewController : UIViewController <UIAlertViewDelegate, UITextFieldDelegate>
+
+
+{
+    int mins;
+    int seconds;
+    int hrs;
+    
+    NSURL *url;
+//    int crashing;
+    
+    
+    
+    IBOutlet UILabel *hourslabel;
+    IBOutlet UILabel *minuteslabel;
+    IBOutlet UILabel *secondslabel;
+    
+    
+    IBOutlet UILabel *hoursTF;
+    IBOutlet UILabel *minsTF;
+    
+    
+    
+    NSTimer *timer;
+    
+    AVAudioPlayer *audio;
+    
+    IBOutlet UIStepper *hourstepper;
+    IBOutlet UIStepper *minutestepper;
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+-(IBAction)settings;
+-(IBAction)resetion;
+-(IBAction)start;
+-(void)countdown;
+//-(void)vibration;
+//-(IBAction)crash;
+-(IBAction)off;
+-(IBAction)hoursstepper;
+-(IBAction)minutesstepper;
+
+
+
+
+
+
 
 @end
--- Alarm mod/SecondViewController.m
+++ Alarm mod/SecondViewController.m
@@ -1,8 +1,8 @@
 //
 //  SecondViewController.m
-//  Alarm mod
+//  alarm for golden week and etc
 //
-//  Created by Jamie Walker on 5/5/14.
+//  Created by Jamie Walker on 5/3/14.
 //  Copyright (c) 2014 Jamie Walker. All rights reserved.
 //
 
@@ -17,7 +17,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"97744^ALARM" ofType:@"mp3"];
+    url = [NSURL fileURLWithPath:path];
+    audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    [audio prepareToPlay];
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
+    hrs = 0;
+    mins = 0;
+    seconds = 0;
+    
+    hourslabel.text=[NSString stringWithFormat:@"%d", hrs];
+    minuteslabel.text=[NSString stringWithFormat:@"%d", mins];
+    secondslabel.text=[NSString stringWithFormat:@"%d", seconds];
+    
+
+//    hoursTF.delegate = self;
+//    minsTF.delegate = self;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +44,180 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(IBAction)settings{
+    
+    hrs = [hoursTF.text intValue];
+    mins = [minsTF.text intValue];
+    seconds = 0 ;
+    
+    
+    
+    hourslabel.text = hoursTF.text;
+    minuteslabel.text = minsTF.text;
+    secondslabel.text = @"0";
+    
+    
+    
+}
+
+
+
+
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    //to close the keyboard thats in ur waaayyyyyyyyy ( IM NOT USING TEXT FIELDS SO......
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+
+-(IBAction)start{
+    
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                             target:self
+                                           selector:@selector(countdown)
+                                           userInfo:nil
+                                            repeats:YES];
+    
+    
+    
+    
+}
+
+
+ 
+
+
+
+
+-(void)countdown{
+    
+    seconds -= 1;
+    
+    if (mins ==0 && seconds == 0 && hrs == 0) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"XD"
+                                                        message:@"WAKE UP!"
+                                                       delegate:nil
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:@"Okay, okay, IM GETTING UP NOW", nil
+                              
+                              ];
+        [alert show];
+        [audio play];
+        
+        
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+        
+        
+        
+            AVCaptureSession *captureSession;
+            [captureSession startRunning];
+            NSError *error = nil;
+            AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+            [captureDevice lockForConfiguration:&error];
+            captureDevice.torchMode = AVCaptureTorchModeOn;
+            [captureDevice unlockForConfiguration];
+        
+        
+        
+        
+        
+        
+        [timer invalidate]; //it stops the timer
+        //        if (AudioServicesPlaySystemSound(kSystemSoundID_Vibrate) isValid) {
+        //        [ audioServicesPlaySystemSound(kSystemSoundID_Vibrate) invalidate];
+        //        }
+        
+        
+    }
+    
+    
+    if ( seconds < 0){
+        mins -= 1;
+        seconds = 59;
+        
+        if (mins < 0) {
+            hrs -= 1;
+            mins = 59;
+        }
+        
+    }
+    
+    
+    secondslabel.text = [NSString stringWithFormat:@"%d", seconds];
+    minuteslabel.text = [NSString stringWithFormat:@"%d", mins];
+    hourslabel.text = [NSString stringWithFormat:@"%d", hrs];
+    
+    
+    
+}
+
+
+
+
+-(IBAction)resetion{
+    // I NEED TO ADD STUFF HERE REMEMBER THAT.
+    hrs = 0;
+    mins = 0;
+    seconds =0;
+    minuteslabel.text = @"0";
+    secondslabel.text = @"0";
+    hourslabel.text = @"0";
+//    hoursTF = 0;
+//    minsTF = 0;
+    [timer invalidate];
+    
+    
+}
+//-(IBAction)crash{
+//    crashing /= 0;
+//    
+//}
+
+
+- (IBAction)hourstepper:(UIStepper *)sender {
+    
+    double value = [sender value];
+    
+    [hoursTF setText:[NSString stringWithFormat:@"%d", (int)value]];
+}
+- (IBAction)minutesstepper{
+    
+    mins = minutestepper.value;
+    minsTF.text=[NSString stringWithFormat:@"%d",mins];
+
+}
+- (IBAction)hoursstepper{
+    
+    hrs = hourstepper.value;
+    hoursTF.text=[NSString stringWithFormat:@"%d",hrs];
+    
+}
+//
+//-(IBAction)off{
+//    NSError *offerror = nil;
+//    AVCaptureDevice *offcaptureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+//    
+//    [offcaptureDevice lockForConfiguration:&offerror];
+//    offcaptureDevice.torchMode = AVCaptureTorchModeOff;
+//    [offcaptureDevice unlockForConfiguration];
+//    
+//    
+//    [captureSession stopRunning];
+//    
+//}
+
+
+
+//-(void)steppers{
+//    hourstepper = hoursTF.text;
+//    minutestepper = minsTF.text;
+//}
+
 
 @end
